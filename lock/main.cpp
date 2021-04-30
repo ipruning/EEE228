e8502846738f7d9f4cfe27a5eb4c4512003b6df3
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-30 07:49:40 +0100

    ver 0.03

--- lock/main.cpp
+++ lock/main.cpp
@@ -9,24 +9,24 @@ char user_input_buffer[10] = {0};
 int user_input_counter = 0;
 char output[] = "0000";
 
-Ticker timer_scan_column;
-Ticker timer_led;
 SLCD display;
-Timer t;
+Ticker ticker_scan_column;
+Ticker ticker_led;
+Timer timer_input;
 
-void AppendBuffer(char input_array[10], int *input, int *input_counter)
+void AppendBuffer(char input_array[10], int input, int *input_counter)
 {
     for (int i = 0; i < *input_counter; i++)
     {
         input_array[*input_counter - i] = input_array[*input_counter - 1 - i];
     }
-    input_array[0] = *input;
+    input_array[0] = input;
     *input_counter = *input_counter + 1;
 }
 
-void DisplayInput(char input_array[10], int *input_counter)
+void DisplayInput(char input_array[10], int input_counter)
 {
-    if (*input_counter >= 4)
+    if (input_counter >= 4)
     {
         for (int i = 0; i < 4; i++)
         {
@@ -35,7 +35,7 @@ void DisplayInput(char input_array[10], int *input_counter)
     }
     else
     {
-        for (int i = 0; i < *input_counter; i++)
+        for (int i = 0; i < input_counter; i++)
         {
             output[3 - i] = input_array[i];
         }
@@ -45,12 +45,13 @@ void DisplayInput(char input_array[10], int *input_counter)
 
 int main()
 {
-    timer_scan_column.attach(ScanColumn, SCAN_COLUMN_PERIOD);
-    timer_led.attach(FlashGreenLED, 1000ms);
+    ticker_scan_column.attach(ScanColumn, SCAN_COLUMN_PERIOD);
+    ticker_led.attach(FlashGreenLED, 1000ms);
     long begin = 0;
     long end = 0;
 
-    while (true) {
+    while (true)
+    {
         user_input = GetInput();
         ThisThread::sleep_for(10ms);
         if (row_1 * row_2 * row_3 * row_4 == 0)
@@ -61,21 +62,21 @@ int main()
                 {
                     user_input_counter = 0;
                 }
-                AppendBuffer(user_input_buffer, &user_input, &user_input_counter);
-                DisplayInput(user_input_buffer, &user_input_counter);
-                t.reset();
+                AppendBuffer(user_input_buffer, user_input, &user_input_counter);
+                DisplayInput(user_input_buffer, user_input_counter);
+                timer_input.reset();
             }
             else
             {
-                end = t.read_us();
-                t.reset();
-                t.start();
+                end = timer_input.read_us();
+                timer_input.reset();
+                timer_input.start();
                 if ((end - begin) > 100000)
                 {
-                    AppendBuffer(user_input_buffer, &user_input, &user_input_counter);
-                    DisplayInput(user_input_buffer, &user_input_counter);
+                    AppendBuffer(user_input_buffer, user_input, &user_input_counter);
+                    DisplayInput(user_input_buffer, user_input_counter);
                 }
-                begin = t.read_us();
+                begin = timer_input.read_us();
             }
         }
     }
