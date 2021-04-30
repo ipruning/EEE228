f2afd374e529a2d5cb88cd6bc177d96a0d7cc34d
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-04-30 02:35:30 +0100

    init

--- lock/main.cpp
+++ lock/main.cpp
@@ -1,18 +1,9 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-
-// Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
-
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
 
     while (true) {
