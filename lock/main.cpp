b2155eb468f8a2aef7cf98acae534c40d2599bbc
Alex <30626291+Spehhhhh@users.noreply.github.com>
2021-05-04 04:18:19 +0100

    add PW Class

--- lock/main.cpp
+++ lock/main.cpp
@@ -1,17 +1,82 @@
 #include "main.h"
-// #include <iostream>
-// #import <string>
+#include <iostream>
+#include <string>
 
-// class PW
+class PW
+{
+public:
+    // int GetPassword(void);
+    void UpdatePassword(char[], int);
+    bool CheckPassword(void);
+    unsigned long long GetHash(char[], int);
+
+    char user_input_buffer[MAX_PW_LENGTH];
+    int user_input_counter;
+    int password_length;
+    unsigned long long password_hash;
+    PW();
+
+private:
+    char salt[11];
+    // char password[MAX_PW_LENGTH];
+};
+
+PW::PW(void)
+{
+    char password_default[MAX_PW_LENGTH] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '9'};
+    // strcpy(password, password_default);
+    password_length = 10;
+
+    strcpy(salt, "jf7@qLy_u6Q*");
+
+    UpdatePassword(password_default, password_length);
+}
+
+// int PW::GetPassword(void)
 // {
-//     public:
-//         double a;
-//         void MyTest();
+//     return password[0];
 // }
-// void PW::MyTest(void)
+
+void PW::UpdatePassword(char input[MAX_PW_LENGTH], int input_length)
+{
+    password_hash = GetHash(input, input_length);
+}
+
+bool PW::CheckPassword(void)
+{
+    if (password_length != user_input_counter)
+    {
+        return false;
+    }
+    else
+    {
+        if (GetHash(user_input_buffer, user_input_counter) == password_hash)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+// void PW::Reset()
 // {
-//     return 0;
 // }
+
+unsigned long long PW::GetHash(char input[MAX_PW_LENGTH], int input_length)
+{
+    std::hash<std::string> hash;
+
+    std::string str;
+    for (int i = 0; i < input_length; i++)
+    {
+        str += std::to_string(input[i]);
+    }
+
+    return hash(strcat(&str[0], salt));
+}
 
 int main()
 {
